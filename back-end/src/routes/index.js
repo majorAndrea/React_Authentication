abcaf7e84dbbe899a01fdecb0d8b488bd2410ff3
majorAndry <majorandry@yahoo.com>
2021-08-08 23:39:30 +0200

    Starts implementing google OAuth

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,17 +1,19 @@
-import { testRoute } from './testRoute';
+import { testRoute } from "./testRoute";
 import { signupRoute } from "./signupRoute";
 import { loginRoute } from "./loginRoute";
 import { updateUserInfoRoute } from "./updateUserInfoRoute";
 import { verifyEmailRoute } from "./verifyEmailRoute";
 import { forgotPasswordRoute } from "./forgotPasswordRoute";
 import { resetPasswordRoute } from "./resetPasswordRoute";
+import { getGoogleOauthUrlRoute } from "./getGoogleOauthUrlRoute";
 
 export const routes = [
-    testRoute,
-    signupRoute,
-    loginRoute,
-    updateUserInfoRoute,
-    verifyEmailRoute,
-    forgotPasswordRoute,
-    resetPasswordRoute,
+  testRoute,
+  signupRoute,
+  loginRoute,
+  updateUserInfoRoute,
+  verifyEmailRoute,
+  forgotPasswordRoute,
+  resetPasswordRoute,
+  getGoogleOauthUrlRoute,
 ];
--- front-end/src/pages/LoginPage.jsx
+++ front-end/src/pages/LoginPage.jsx
@@ -3,71 +3,89 @@ import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { useToken } from "../auth/useToken";
 
-
 export const LoginPage = () => {
-	const [token, setToken] = useToken();
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [errorMessage, setErrorMessage] = useState(null);
-	const history = useHistory();
+  const [token, setToken] = useToken();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [googleOauthUrl, setGoogleOauthUrl] = useState("");
+  const history = useHistory();
 
-	const onLoginClicked = async () => {
-		try {
-			const response = await axios.post("/api/login", {
-				email,
-				password
-			});
-			const { token } = response.data;
+  const onLoginClicked = async () => {
+    try {
+      const response = await axios.post("/api/login", {
+        email,
+        password,
+      });
+      const { token } = response.data;
 
-			setToken(token);
-			history.push("/");
-		} catch (e) {
-			setErrorMessage("Invalid credentials!");
-		}
-	}
+      setToken(token);
+      history.push("/");
+    } catch (e) {
+      setErrorMessage("Invalid credentials!");
+    }
+  };
 
-	useEffect(() => {
-		if (errorMessage) {
-			setTimeout(() => {
-				setErrorMessage(null);
-			}, 3000);
-		}
-	}, [errorMessage]);
+  useEffect(() => {
+    if (errorMessage) {
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 3000);
+    }
+  }, [errorMessage]);
 
-	return (
-		<div className="content-container">
-			<h1>Login</h1>
-			{errorMessage && <div className="fail">{errorMessage}</div>}
+  useEffect(() => {
+    const loadOauthUrl = async () => {
+      try {
+        const response = await axios.get("/auth/google/url");
+        const { url } = response.data;
+        setGoogleOauthUrl(url);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-			<input 
-				placeholder="your-email@email.com"
-				value={email}
-				onChange={(e) => setEmail(e.target.value)}
-			/>
-			<input
-				type="password"
-				placeholder="password"
-				value={password}
-				onChange={(e) => setPassword(e.target.value)}
-			/>
-			<hr />
+    loadOauthUrl();
+  }, []);
 
-			<button 
-				onClick={onLoginClicked}
-				disabled={!password || !email}
-			>
-			Log In
-			</button>
-			<button
-				onClick={() => history.push("/forgot-password")}
-			>
-			Forgot your password?
-			</button>
-			<button
-				onClick={() => history.push("/signup")}
-			>
-			Don't have an account? Sign Up
-			</button>
-		</div>
-	);
-}
+  return (
+    <div className="content-container">
+      <h1>Login</h1>
+      {errorMessage && <div className="fail">{errorMessage}</div>}
+
+      <input
+        placeholder="your-email@email.com"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <hr />
+
+      <button onClick={onLoginClicked} disabled={!password || !email}>
+        Log In
+      </button>
+      <button onClick={() => history.push("/forgot-password")}>
+        Forgot your password?
+      </button>
+      <button onClick={() => history.push("/signup")}>
+        Don't have an account? Sign Up
+      </button>
+      <div
+        disabled={!googleOauthUrl}
+        className="googleBtn customBtn"
+        role="button"
+        onClick={() => {
+          window.location.href = googleOauthUrl;
+        }}
+      >
+        <span className="icon"></span>
+        <span className="buttonText">Sign in with Google</span>
+      </div>
+    </div>
+  );
+};
